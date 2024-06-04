39693e6edb8cddb6b3853a99c58f6ee6f10393d6
pasinduWick <pasindu@zkewed.com>
2024-06-04 17:46:09 +0530

    remove verifivations

--- DMT/dmtsite/urls.py
+++ DMT/dmtsite/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [ path("", views.valuation, name="valuation"),
                 path("valuation", views.valuation, name="valuation"),
-                path("verification", views.Verification, name="verification"),
+                # path("verification", views.Verification, name="verification"),
                 # path("RPA", views.RPA, name="RPA"),
                 # path("moderate", views.moderate, name = "moderate")
                 
