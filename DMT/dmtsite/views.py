1459919b2d372080a8d6fa67a1c8f3e9bd3f1632
pasinduWick <pasindu@zkewed.com>
2024-07-05 11:32:40 +0530

    reverse

--- DMT/dmtsite/views.py
+++ DMT/dmtsite/views.py
@@ -313,10 +313,10 @@ def valuation(request):
             vehicle_year = collection.distinct('year')
             vehicle_full_db = list(collection.find({}))
 
-            # vehicle_list = [{'category': obj['category'], 'make': obj['make'], 'model': obj['model'], 'year': obj['year']} for obj in vehicle_full_db]
-            # # print(len(vehicle_list))
+            vehicle_list = [{'category': obj['category'], 'make': obj['make'], 'model': obj['model'], 'year': obj['year']} for obj in vehicle_full_db]
+            # print(len(vehicle_list))
             
-            # vehicle_details = list({v["model"]:v for v in vehicle_list}.values())
+            vehicle_details = list({v["model"]:v for v in vehicle_list}.values())
             # print(vehicle_make)
             # print(len(vehicle_details))
             
@@ -349,7 +349,7 @@ def valuation(request):
 
                                                     'vehicle_make': vehicle_make,
                                                     
-                                                    # 'vehicle_details': vehicle_details,
+                                                    'vehicle_details': vehicle_details,
 
                                                     'vehicle_model': vehicle_model,
 
